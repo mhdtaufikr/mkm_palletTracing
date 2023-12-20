1a097c8f1267f3a91837f589ac9b8f36923d0af0
mhdtaufikrm <mtaufikramadhan27@gmail.com>
2023-12-20 21:14:23 +0700

    done dev template

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.master')
+
+@section('content')
+<main>
+    <header class="page-header page-header-dark bg-gradient-primary-to-secondary pb-10">
+        <div class="container-xl px-4">
+            <div class="page-header-content pt-4">
+                <div class="row align-items-center justify-content-between">
+                    <div class="col-auto mt-2">
+                        <h1 class="page-header-title">
+                            {{-- <div class="page-header-icon"><i data-feather="file"></i></div> --}}
+                            <label id="lblGreetings"></label>
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Main page content-->
+    {{-- <div class="container-xl px-4 mt-n10">
+        <div class="card">
+            {{-- <div class="card-header">Example Card</div> --}}
+            <div class="card-body">
+                
+            </div>
+        </div>
+    </div> --}}
+    <script>
+        var myDate = new Date();
+        var hrs = myDate.getHours();
+
+        var greet;
+
+        if (hrs < 12)
+            greet = 'Good Morning';
+        else if (hrs >= 12 && hrs <= 17)
+            greet = 'Good Afternoon';
+        else if (hrs >= 17 && hrs <= 24)
+            greet = 'Good Evening';
+
+        document.getElementById('lblGreetings').innerHTML =
+            '<b>' + greet + '</b> and welcome to Pallet Tracing!';
+    </script>
+</main>
+@endsection
