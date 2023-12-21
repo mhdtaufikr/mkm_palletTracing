1f8b5a081d54c47bd725b556e7af6c43dd5453f7
mhdtaufikr <mtaufikramadhan272gmail.com>
2023-12-21 09:35:18 +0700

    fix index and home add another type pallet

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,35 +8,37 @@ use App\Models\Pallet;
 class HomeController extends Controller
 {
     public function index()
-    {
-        // Fetch pallet data
-        $palletData = Pallet::get();
-    
-        // Initialize counts array
-        $counts = [];
-    
-        // Loop through the pallet data
-        foreach ($palletData as $pallet) {
-            $typePallet = $pallet->type_pallet;
-            $destination = $pallet->destination;
-    
-            if (!isset($counts[$typePallet])) {
-                $counts[$typePallet] = [];
-            }
-    
-            if (!isset($counts[$typePallet][$destination])) {
-                $counts[$typePallet][$destination] = 1;
-            } else {
-                $counts[$typePallet][$destination]++;
-            }
+{
+    // Fetch pallet data
+    $palletData = Pallet::get();
+
+    // Initialize counts array
+    $counts = [];
+
+    // Loop through the pallet data
+    foreach ($palletData as $pallet) {
+        $typePallet = $pallet->type_pallet;
+        $destination = $pallet->destination;
+
+        if (!isset($counts[$typePallet])) {
+            $counts[$typePallet] = [];
         }
-    
-        // Pass the data to the view
-        return view('home.index', [
-            'enginePieData' => json_encode($counts['Engine'] ?? []),
-            'transmissionPieData' => json_encode($counts['Transmission'] ?? []),
-        ]);
+
+        if (!isset($counts[$typePallet][$destination])) {
+            $counts[$typePallet][$destination] = 1;
+        } else {
+            $counts[$typePallet][$destination]++;
+        }
     }
+
+    // Pass the data to the view
+    return view('home.index', [
+        'enginePieData' => json_encode($counts['Engine'] ?? []),
+        'transmissionPieData' => json_encode($counts['TM-Assy'] ?? []),
+        'faPieData' => json_encode($counts['FA'] ?? []), // Add this line
+    ]);
+}
+
     
 }
 
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -20,79 +20,105 @@
     <div class="row">
 
         <!-- Pie chart for 'Engine' -->
-        <div class="col-md-6">
+        <div class="col-md-4">
             <div class="card mb-4">
                 <div class="card-header text-dark">Pallet Engine</div>
                 <div class="card-body">
                     <div class="chart-pie"><canvas id="enginePieChart" width="100%" height="50"></canvas></div>
                 </div>
                <div class="card-footer small text-muted">Updated today at {{ now()->format('h:i A') }}</div>
-
             </div>
         </div>
 
         <!-- Pie chart for 'Transmission' -->
-        <div class="col-md-6">
+        <div class="col-md-4">
             <div class="card mb-4">
-                <div class="card-header text-dark">Pallet Transmission</div>
+                <div class="card-header text-dark">Pallet TM-Assy</div>
                 <div class="card-body">
                     <div class="chart-pie"><canvas id="transmissionPieChart" width="100%" height="50"></canvas></div>
                 </div>
                <div class="card-footer small text-muted">Updated today at {{ now()->format('h:i A') }}</div>
+            </div>
+        </div>
 
+        <!-- Pie chart for 'FA' -->
+        <div class="col-md-4">
+            <div class="card mb-4">
+                <div class="card-header text-dark">Pallet FA</div>
+                <div class="card-body">
+                    <div class="chart-pie"><canvas id="faPieChart" width="100%" height="50"></canvas></div>
+                </div>
+               <div class="card-footer small text-muted">Updated today at {{ now()->format('h:i A') }}</div>
             </div>
         </div>
 
     </div>
 </div>
 
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    // Data for 'Engine' Pie Chart
+    var enginePieData = {!! $enginePieData !!};
 
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script>
-        var myDate = new Date();
-        var hrs = myDate.getHours();
+    // Data for 'Transmission' Pie Chart
+    var transmissionPieData = {!! $transmissionPieData !!};
 
-        var greet;
+    // Data for 'FA' Pie Chart
+    var faPieData = {!! $faPieData !!};
 
-        if (hrs < 12)
-            greet = 'Good Morning';
-        else if (hrs >= 12 && hrs <= 17)
-            greet = 'Good Afternoon';
-        else if (hrs >= 17 && hrs <= 24)
-            greet = 'Good Evening';
+    // Initialize 'Engine' Pie Chart
+    var enginePieChart = new Chart(document.getElementById('enginePieChart').getContext('2d'), {
+        type: 'pie',
+        data: {
+            labels: Object.keys(enginePieData),
+            datasets: [{
+                data: Object.values(enginePieData),
+                backgroundColor: ['#FF5733', '#FFC300', '#33FF57'], // Set your preferred colors
+            }],
+        },
+    });
 
-        document.getElementById('lblGreetings').innerHTML =
-            '<b>' + greet + '</b> and welcome to Pallet Tracing!';
+    // Initialize 'Transmission' Pie Chart
+    var transmissionPieChart = new Chart(document.getElementById('transmissionPieChart').getContext('2d'), {
+        type: 'pie',
+        data: {
+            labels: Object.keys(transmissionPieData),
+            datasets: [{
+                data: Object.values(transmissionPieData),
+                backgroundColor: ['#FF5733', '#FFC300', '#33FF57'], // Set your preferred colors
+            }],
+        },
+    });
 
-        // Data for 'Engine' Pie Chart
-        var enginePieData = {!! $enginePieData !!};
+    // Initialize 'FA' Pie Chart
+    var faPieChart = new Chart(document.getElementById('faPieChart').getContext('2d'), {
+        type: 'pie',
+        data: {
+            labels: Object.keys(faPieData),
+            datasets: [{
+                data: Object.values(faPieData),
+                backgroundColor: ['#FF5733', '#FFC300', '#33FF57'], // Set your preferred colors
+            }],
+        },
+    });
+</script>
 
-        // Data for 'Transmission' Pie Chart
-        var transmissionPieData = {!! $transmissionPieData !!};
+<script>
+    var myDate = new Date();
+    var hrs = myDate.getHours();
 
-        // Initialize 'Engine' Pie Chart
-        var enginePieChart = new Chart(document.getElementById('enginePieChart').getContext('2d'), {
-            type: 'pie',
-            data: {
-                labels: Object.keys(enginePieData),
-                datasets: [{
-                    data: Object.values(enginePieData),
-                    backgroundColor: ['#FF5733', '#FFC300', '#33FF57'], // Set your preferred colors
-                }],
-            },
-        });
+    var greet;
 
-        // Initialize 'Transmission' Pie Chart
-        var transmissionPieChart = new Chart(document.getElementById('transmissionPieChart').getContext('2d'), {
-            type: 'pie',
-            data: {
-                labels: Object.keys(transmissionPieData),
-                datasets: [{
-                    data: Object.values(transmissionPieData),
-                    backgroundColor: ['#FF5733', '#FFC300', '#33FF57'], // Set your preferred colors
-                }],
-            },
-        });
-    </script>
+    if (hrs < 12)
+        greet = 'Good Morning';
+    else if (hrs >= 12 && hrs <= 17)
+        greet = 'Good Afternoon';
+    else if (hrs >= 17 && hrs <= 24)
+        greet = 'Good Evening';
+
+    document.getElementById('lblGreetings').innerHTML =
+        '<b>' + greet + '</b> and welcome to Pallet Tracing!';
+</script>
+
 </main>
 @endsection
